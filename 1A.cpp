e90bd3abb72ec60ca454b5d96ada53e3d23d7f4c
molidex <kakakakaka2018@mail.ru>
2021-11-22 14:57:51 -0500

    chad A task

--- 1A.cpp
+++ 1A.cpp
@@ -10,110 +10,176 @@ using std::stack;
 using std::cin;
 using std::cout;
 
-typedef int elem_t;
+typedef int Vertex;
 
-const elem_t start = -3802;
+const Vertex kStart = -3802;
+
+//=======IGraph=======//
 
 class IGraph
 {
 public:
 
-    virtual void Input() = 0;
+    virtual void addEdge(const Vertex& from, const Vertex& to) = 0;
+    virtual const vector<Vertex>& getNeighbors(const Vertex& vertex) const = 0;
+    virtual size_t getAmountVertices() const = 0;
+    virtual size_t getAmountEdges() const = 0;
 };
 
-class Graph : public IGraph
+//=======ListGraph=======//
+
+class ListGraph : public IGraph
 {
 private:
 
     size_t amount_vertices_;
     size_t amount_edges_;
 
-    vector<vector<elem_t>> graph;
-
-    void makeUpPaths(elem_t start_vertex, 
-                 vector<bool>& visited, 
-                 vector<elem_t>& parents)
-    {
-        queue<elem_t> vertex_queue;
-
-        visited[start_vertex] = true;
-        parents[start_vertex] = start;
-        vertex_queue.push(start_vertex);
-        while (!vertex_queue.empty())
-        {
-            size_t current_vertex = vertex_queue.front();
-            vertex_queue.pop();
-            for (size_t i = 0; i < graph[current_vertex].size(); ++i)
-            {
-                size_t vertex_to = graph[current_vertex][i];
-                if (!visited[vertex_to])
-                {
-                    visited[vertex_to] = true;
-                    vertex_queue.push(vertex_to);
-                    parents[vertex_to] = current_vertex;
-                }
-            }
-        }
-    }
-
-    //note: path must be exist!
-    void theNearestPathToTarget(elem_t target_vertex,
-                                vector<elem_t>& path,
-                                vector<elem_t>& parents)
-    {   
-        elem_t current_vertex = target_vertex;
-        while (current_vertex != start)
-        {
-            path.push_back(current_vertex);
-            current_vertex = parents[current_vertex];
-        }
-    }
+    vector<vector<Vertex>> graph_;
 
 public:
 
-    explicit Graph(size_t amount_vertices, size_t amount_edges) 
+    explicit ListGraph(size_t amount_vertices, size_t amount_edges)
         : amount_vertices_(amount_vertices)
         , amount_edges_(amount_edges)
-        , graph(amount_vertices, vector<elem_t>())
+        , graph_(amount_vertices_, vector<Vertex>())
         {}
     
-    void Input() override 
+    void addEdge(const Vertex& from, const Vertex& to) override
     {
-        elem_t vertex_a = 0;
-        elem_t vertex_b = 0;
-        for (size_t i = 0; i < amount_edges_; ++i)
-        {   
-            cin >> vertex_a >> vertex_b;
-
-            graph[vertex_a - 1].push_back(vertex_b - 1);
-            graph[vertex_b - 1].push_back(vertex_a - 1);
-        }
+        graph_[from].push_back(to);
     }
 
-    vector<elem_t> getTheNearestPath(elem_t start_vertex, elem_t end_vertex)
+    const vector<Vertex>& getNeighbors(const Vertex& vertex) const override
     {
-        vector<bool> visited(amount_vertices_);
-        vector<elem_t> parents(amount_vertices_);
-        vector<elem_t> path;
-
-        makeUpPaths(start_vertex, visited, parents);
-
-        if (visited[end_vertex - 1])
-        {
-            theNearestPathToTarget(end_vertex, path, parents);
-        }
-        return path;
+        return graph_[vertex];
     }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
 };
 
+//=======MatrixGraph=======//
 
-void printPath(vector<elem_t> path)
+class MatrixGraph : public IGraph
+{
+private:
+
+    vector<vector<Vertex>> graph_;
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+public:
+
+    explicit MatrixGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices, vector<Vertex>(amount_vertices))
+    {   
+        // благодаря этому, мне не нужно создавать новый вектор и делать лишние копирования
+        // для того, чтобы bfs работал корректно
+        Vertex vertex = 0;
+        for (auto& edges : graph_)
+        {
+            std::fill(begin(edges), end(edges), vertex++);
+        }
+    }
+
+    void addEdge(const Vertex& from, const Vertex& to) override
+    {   
+        // благодаря этому, мне не нужно создавать новый вектор и делать много копий вектора
+        // для того, чтобы bfs работал корректно
+        // но, да, мы не можем теперь хранить расстояние в этой ячейке, за полиморфизм нужно платить.
+        // В данном случае это память.
+        graph_[from][to] = to;
+    }
+
+    const vector<Vertex>& getNeighbors(const Vertex& vertex) const override
+    {
+        return graph_[vertex];
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======GraphAlgorithms=======//
+
+void BFS(const IGraph& graph, Vertex start, vector<bool>& visited, vector<Vertex>& parents)
+{
+    queue<Vertex> queue;
+
+    visited[start] = true;
+    parents[start] = kStart;
+    
+    queue.push(start);
+    while (!queue.empty())
+    {
+        Vertex current = queue.front();
+        queue.pop();
+
+        for (Vertex to : graph.getNeighbors(current))
+        {
+            if (!visited[to])
+            {
+                visited[to] = true;
+
+                queue.push(to);
+                
+                parents[to] = current;
+            }
+        }
+    }
+}
+
+void initOptimalPath(Vertex target_vertex,
+                     vector<Vertex>& path,
+                     const vector<Vertex>& parents)
+{   
+    Vertex current_vertex = target_vertex;
+    while (current_vertex != kStart)
+    {
+        path.push_back(current_vertex);
+        current_vertex = parents[current_vertex];
+    }
+}
+
+vector<Vertex> getOptimalPath(const IGraph& graph, Vertex start, Vertex end)
+{
+    vector<bool> visited(graph.getAmountVertices());
+    vector<Vertex> parents(graph.getAmountVertices());
+    vector<Vertex> path;
+
+    BFS(graph, start, visited, parents);
+
+    if (visited[end])
+    {
+        initOptimalPath(end, path, parents);
+    }
+    return path;
+}
+
+void inputGraphByEdgeList(IGraph& graph)
+{
+    Vertex vertex_a = 0;
+    Vertex vertex_b = 0;
+    for (size_t i = 0; i < graph.getAmountEdges(); ++i)
+    {   
+        cin >> vertex_a >> vertex_b;
+
+        graph.addEdge(vertex_a - 1, vertex_b - 1);
+        graph.addEdge(vertex_b - 1, vertex_a - 1);
+    }
+}
+
+void printPath(const vector<Vertex>& path)
 {   
     cout << '\n' << path.size() - 1 << '\n';
 
-    for (int i = path.size() - 1; i > - 1; ++i)
+    for (int i = path.size() - 1; i > - 1; --i)
     {
-        cout << path[i] + 1 << '\n';
+        cout << path[i] + 1 << ' ';
     }
     cout << '\n';
 }
@@ -122,18 +188,21 @@ void Processing()
 {
     size_t amount_vertices = 0;
     size_t amount_edges = 0;
-    elem_t start_vertex = 0;
-    elem_t target_vertex = 0;
+    Vertex start_vertex = 0;
+    Vertex target_vertex = 0;
 
     cin >> amount_vertices >> amount_edges;
     cin >> start_vertex >> target_vertex;
 
-    Graph graph(amount_vertices, amount_edges);
-    graph.Input();
+    // MatrixGraph graph(amount_vertices, amount_edges);
+    ListGraph graph(amount_vertices, amount_edges);
 
-    vector<elem_t> path = graph.getTheNearestPath(start_vertex - 1, target_vertex - 1);
+    inputGraphByEdgeList(graph);
 
-    printPath(path);
+    vector<Vertex> path = getOptimalPath(graph, start_vertex - 1, target_vertex - 1);
+
+    if (path.size() == 0) cout << "-1\n";
+    else printPath(path);
 }
 
 int main()

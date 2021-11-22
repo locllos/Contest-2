688b49b9485a112946d265a30ef96402741aa8f9
molidex <kakakakaka2018@mail.ru>
2021-11-22 16:25:57 -0500

    chad E task

--- 1E.cpp
+++ 1E.cpp
@@ -14,50 +14,133 @@ using std::stack;
 using std::queue;
 using std::fill;
 
-struct ExtentedVertex 
+
+typedef int Vertex;
+
+const Vertex kStart = -3802;
+
+//=======IGraph=======//
+
+class IGraph
 {
-    int number;
-    bool is_sub = false;
+public:
+
+    virtual void addEdge(const Vertex& from, const Vertex& to) = 0;
+    virtual const vector<Vertex>& getNeighbors(const Vertex& vertex) const = 0;
+    virtual size_t getAmountVertices() const = 0;
+    virtual size_t getAmountEdges() const = 0;
 };
 
-typedef ExtentedVertex elem_t;
+//=======ListGraph=======//
 
-const int start = -1;
+class ListGraph : public IGraph
+{
+private:
 
-void InputStrangeGraph(int height, int length, 
-                       vector<vector<elem_t>>& graph,
-                       queue<int>& vertex_queue,
-                       vector<bool>& visited)
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+    vector<vector<Vertex>> graph_;
+
+public:
+
+    explicit ListGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices_, vector<Vertex>())
+        {}
+    
+    void addEdge(const Vertex& from, const Vertex& to) override
+    {
+        graph_[from].push_back(to);
+    }
+
+    const vector<Vertex>& getNeighbors(const Vertex& vertex) const override
+    {
+        return graph_[vertex];
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======MatrixGraph=======//
+
+class MatrixGraph : public IGraph
+{
+private:
+
+    vector<vector<Vertex>> graph_;
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+public:
+
+    explicit MatrixGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices, vector<Vertex>(amount_vertices))
+    {   
+        // благодаря этому, мне не нужно создавать новый вектор и делать лишние копирования
+        // для того, чтобы bfs работал корректно
+        Vertex vertex = 0;
+        for (auto& edges : graph_)
+        {
+            std::fill(begin(edges), end(edges), vertex++);
+        }
+    }
+
+    void addEdge(const Vertex& from, const Vertex& to) override
+    {   
+        // благодаря этому, мне не нужно создавать новый вектор и делать много копий вектора
+        // для того, чтобы bfs работал корректно
+        // но, да, мы не можем теперь хранить расстояние в этой ячейке, за полиморфизм нужно платить.
+        // В данном случае это память.
+        graph_[from][to] = to;
+    }
+
+    const vector<Vertex>& getNeighbors(const Vertex& vertex) const override
+    {
+        return graph_[vertex];
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======GraphAlgorithms=======//
+
+void Input(int height, int length, 
+           IGraph& graph,
+           vector<bool>& is_sub)
 {      
-    bool is_sub = false;
     int current_x = 0;
     int current_y = 0;
-    ExtentedVertex current_vertex = {};
     while (current_y < height)
     {
-        cin >> is_sub;
-        current_vertex.number = current_y * length + current_x;
-        current_vertex.is_sub = is_sub;
-        if (is_sub)
-        {
-            vertex_queue.push(current_vertex.number);
-            visited[current_vertex.number] = true;
-        }
+        Vertex current = current_y * length + current_x;
+        
+        bool sub_indicator = false;
+        cin >> sub_indicator; 
+
+        is_sub[current] = sub_indicator;
+
         if (current_x != 0)
-        {
-            graph[current_y * length + current_x - 1].push_back(current_vertex);
+        {   
+            graph.addEdge(current_y * length + current_x - 1, current);
         }
         if (current_x != length - 1)
-        {
-            graph[current_y * length + current_x + 1].push_back(current_vertex);
+        {   
+            graph.addEdge(current_y * length + current_x + 1, current);
         }
         if (current_y != 0)
         {
-            graph[(current_y - 1) * length + current_x].push_back(current_vertex);
+            graph.addEdge((current_y - 1) * length + current_x, current);
         }
         if (current_y != height - 1)
         {
-            graph[(current_y + 1) * length + current_x].push_back(current_vertex);
+            graph.addEdge((current_y + 1) * length + current_x, current);
         }
         ++current_x;
         if (current_x == length)
@@ -68,29 +151,28 @@ void InputStrangeGraph(int height, int length,
     }
 }
 
-void initializeDistances(queue<int>& vertex_queue, 
-                         vector<vector<elem_t>>& graph,
+void initializeDistances(const IGraph& graph,
+                         queue<Vertex>& queue, 
                          vector<bool>& visited, vector<int>& distances)
-{   
-    while (!vertex_queue.empty())
+{
+    while (!queue.empty())
     {
-        int vertex_from = vertex_queue.front();
+        Vertex from = queue.front();
 
-        vertex_queue.pop();
-        for (size_t i = 0; i < graph[vertex_from].size(); ++i)
+        queue.pop();
+        for (auto& to : graph.getNeighbors(from))
         {
-            elem_t vertex_to = graph[vertex_from][i];
-            if (!visited[vertex_to.number])
+            if (!visited[to])
             {
-                visited[vertex_to.number] = true;
-                vertex_queue.push(vertex_to.number);
+                visited[to] = true;
+                
+                queue.push(to);
 
-                distances[vertex_to.number] = distances[vertex_from] + 1;
+                distances[to] = distances[from] + 1;
             }
         }
     }
 }
-
 
 void Processing()
 {
@@ -99,20 +181,30 @@ void Processing()
     cin >> height >> length;
 
     size_t amount_vertices = height * length;
-    vector<bool> are_sub(amount_vertices, false);
-    vector<vector<elem_t>> graph(amount_vertices);
+    
+    ListGraph graph(amount_vertices, amount_vertices + 1);
+    
+    queue<Vertex> queue;
     vector<bool> visited(amount_vertices, false);
+    vector<bool> is_sub(amount_vertices, false);
+    Input(height, length, graph, is_sub);
 
-    queue<int> vertex_queue;
-    InputStrangeGraph(height, length, graph, vertex_queue, visited);
+    for (Vertex current = 0; current < amount_vertices; ++current)
+    {
+        if (is_sub[current])
+        {
+            queue.push(current);
+            visited[current] = true;
+        }
+    }
 
     vector<int> distances(amount_vertices, 0);
-    initializeDistances(vertex_queue, graph, visited, distances);
+    initializeDistances(graph, queue, visited, distances);
 
-    for (size_t i = 0; i < amount_vertices; ++i)
+    for (Vertex current = 0; current < amount_vertices; ++current)
     {   
-        cout << distances[i] << " ";
-        if (i != 0 && (i + 1) % length == 0)
+        cout << distances[current] << " ";
+        if (current != 0 && (current + 1) % length == 0)
         {
             cout << std::endl;
         }

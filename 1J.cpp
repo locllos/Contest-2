aa250c94b3762ffa0a8086ca3b8e3a7c15055119
molidex <kakakakaka2018@mail.ru>
2021-11-22 17:34:16 -0500

    chad H task

--- 1J.cpp
+++ 1J.cpp
@@ -9,52 +9,143 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+typedef int Vertex;
+
 struct Edge
 {   
-    int from;
-    int to;
-    
-    int fine;
+    Vertex from;
+    Vertex to;
+
+    int distance;
 };
 
-using Graph = vector<vector<Edge>>;
+const int kInf = (int)1e9;
+const Vertex kStart = -3802;
 
-void inputGraph(Graph& graph, int amount_edges)
+//=======IGraph=======//
+
+class IGraph
 {
-    int vertex_to = 0;
-    int vertex_from = 0;
-    for (int i = 0; i < amount_edges; ++i)
-    {
-        cin >> vertex_from >> vertex_to;
+public:
 
-        graph[vertex_from - 1].push_back({vertex_from - 1, vertex_to - 1, 0});
+    virtual void addEdge(const Vertex& from, const Vertex& to, int distance) = 0;
+    virtual vector<Edge> getNeighbors(const Vertex& vertex) const = 0;
+    virtual size_t getAmountVertices() const = 0;
+    virtual size_t getAmountEdges() const = 0;
+};
+
+//=======ListGraph=======//
+
+class ListGraph : public IGraph
+{
+private:
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+    vector<vector<Edge>> graph_;
+
+public:
+
+    explicit ListGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices_, vector<Edge>())
+        {}
+    
+    void addEdge(const Vertex& from, const Vertex& to, int distance) override
+    {
+        graph_[from].push_back({from, to, distance});
+    }
+
+    vector<Edge> getNeighbors(const Vertex& vertex) const override
+    {
+        return graph_[vertex];
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======MatrixGraph=======//
+
+class MatrixGraph : public IGraph
+{
+private:
+
+    vector<vector<Vertex>> graph_;
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+public:
+
+    explicit MatrixGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices, vector<Vertex>(amount_vertices, kInf))
+        {}
+
+    void addEdge(const Vertex& from, const Vertex& to, int distance) override
+    {   
+        graph_[from][to] = distance;
+    }
+
+    vector<Edge> getNeighbors(const Vertex& vertex) const override
+    {   
+        vector<Edge> neighbors;
         
-        graph[vertex_to - 1].push_back({vertex_to - 1, vertex_from - 1, 1});
+        for (Vertex to = 0; to < amount_vertices_; ++to)
+        {   
+            if (graph_[vertex][to] < kInf)
+            {
+                neighbors.push_back({vertex, to, graph_[vertex][to]});
+            }
+        }
+        return neighbors;
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+
+void inputGraph(IGraph& graph)
+{
+    Vertex to = 0;
+    Vertex from = 0;
+
+    for (int i = 0; i < graph.getAmountEdges(); ++i)
+    {
+        cin >> from >> to;
+
+        graph.addEdge(from - 1, to - 1, 0);
+        graph.addEdge(to - 1, from - 1, 1);
     }
 }
 
-void calculateFines(Graph& graph, vector<int>& fines,
-                    int start_vertex, int end_vertex)
+void initializeDistances(IGraph& graph, vector<int>& distances,
+                         const Vertex& start, const Vertex& end_vertex)
 {
     deque<int> bi_queue;
-    fines[start_vertex] = 0;
+    distances[start] = 0;
     
-    bi_queue.push_back(start_vertex);
+    bi_queue.push_back(start);
     while(bi_queue.size() > 0)
     {
-        int current_vertex = bi_queue.front();
+        Vertex current = bi_queue.front();
         bi_queue.pop_front();
 
-        if (current_vertex == end_vertex) return;
+        if (current == end_vertex) return;
 
-        for (Edge& edge : graph[current_vertex])
+        for (Edge& edge : graph.getNeighbors(current))
         {
-            if (fines[edge.from] + edge.fine < fines[edge.to])
+            if (distances[edge.from] + edge.distance < distances[edge.to])
             {
-                fines[edge.to] = fines[edge.from] + edge.fine;
+                distances[edge.to] = distances[edge.from] + edge.distance;
 
-                if (edge.fine == 0) bi_queue.push_front(edge.to);
-                else                bi_queue.push_back(edge.to);
+                if (edge.distance == 0) bi_queue.push_front(edge.to);
+                else                    bi_queue.push_back(edge.to);
             }
         }
     }
@@ -69,8 +160,8 @@ int main()
     int amount_edges = 0;
     cin >> amount_vertices >> amount_edges;
 
-    Graph graph(amount_vertices);
-    inputGraph(graph, amount_edges);
+    ListGraph graph(amount_vertices, amount_edges);
+    inputGraph(graph);
 
     int amount_requests = 0;
     cin >> amount_requests;
@@ -78,12 +169,12 @@ int main()
     vector<int> fines(amount_vertices, amount_vertices + 1);
     for (int i = 0; i < amount_requests; ++i)
     {
-        int start = 0;
-        int end = 0;
+        Vertex start = 0;
+        Vertex end = 0;
 
         cin >> start >> end;
 
-        calculateFines(graph, fines, start - 1, end - 1);
+        initializeDistances(graph, fines, start - 1, end - 1);
 
         cout << ((fines[end - 1] == amount_vertices + 1) ? -1 : fines[end - 1]) << endl;
 

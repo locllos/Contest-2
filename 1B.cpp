1e6441b5d83889d7a0f03a913f4482b00991bb0b
molidex <kakakakaka2018@mail.ru>
2021-11-22 15:28:02 -0500

    chad B task

--- 1B.cpp
+++ 1B.cpp
@@ -8,97 +8,160 @@ using std::stack;
 using std::cin;
 using std::cout;
 
+using Vertex = int;
+
+enum Color
+{
+    WHITE,
+    GRAY,
+    BLACK,
+
+    AMOUNT_COLORS,
+};
+
+
+//=======IGraph=======//
 
 class IGraph
 {
 public:
 
-    virtual void Input() = 0;
+    virtual void addEdge(const Vertex& from, const Vertex& to) = 0;
+    virtual const vector<Vertex>& getNeighbors(const Vertex& vertex) const = 0;
+    virtual size_t getAmountVertices() const = 0;
+    virtual size_t getAmountEdges() const = 0;
 };
 
-class Graph : public IGraph
+//=======ListGraph=======//
+
+class ListGraph : public IGraph
 {
 private:
 
-    size_t amount_edges_;
     size_t amount_vertices_;
+    size_t amount_edges_;
 
-    vector<vector<int>> graph_;
-
-    enum Color
-    {
-        WHITE,
-        GRAY,
-        BLACK,
-
-        AMOUNT_COLORS,
-    };
-
-private:
-
-    bool isSplitable(int start_vertex, vector<Color>& color)
-    {
-        size_t is_splitable = true;
-        for (int current_vertex : graph_[start_vertex])
-        {
-            if (color[current_vertex] == WHITE)
-            {
-                color[current_vertex] = static_cast<Color>(AMOUNT_COLORS - color[start_vertex]);
-                is_splitable = isSplitable(current_vertex, color); 
-            }
-            else if (color[current_vertex] ==
-                     color[start_vertex])
-            {
-                is_splitable = false;
-            }
-        }
-        return is_splitable;
-    }
+    vector<vector<Vertex>> graph_;
 
 public:
 
-    explicit Graph(size_t amount_edges, size_t amount_vertices)
-        : amount_edges_(amount_edges)
-        , amount_vertices_(amount_vertices)
-        , graph_(amount_vertices_, vector<int>())
+    explicit ListGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices_, vector<Vertex>())
         {}
-
-    void Input() override
+    
+    void addEdge(const Vertex& from, const Vertex& to) override
     {
-        int vertex_a = 0;
-        int vertex_b = 0;
-        for (size_t i = 0; i < amount_edges_; ++i)
-        {   
-            cin >> vertex_a >> vertex_b;
+        graph_[from].push_back(to);
+    }
 
-            graph_[vertex_a - 1].push_back(vertex_b - 1);
-            graph_[vertex_b - 1].push_back(vertex_a - 1);
+    const vector<Vertex>& getNeighbors(const Vertex& vertex) const override
+    {
+        return graph_[vertex];
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======MatrixGraph=======//
+
+class MatrixGraph : public IGraph
+{
+private:
+
+    vector<vector<Vertex>> graph_;
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+public:
+
+    explicit MatrixGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices, vector<Vertex>(amount_vertices))
+    {   
+        // благодаря этому, мне не нужно создавать новый вектор и делать лишние копирования
+        // для того, чтобы bfs работал корректно
+        Vertex vertex = 0;
+        for (auto& edges : graph_)
+        {
+            std::fill(begin(edges), end(edges), vertex++);
         }
     }
 
-    bool isSplitable()
-    {
-        vector<Color> color(amount_vertices_, WHITE);
+    void addEdge(const Vertex& from, const Vertex& to) override
+    {   
+        // благодаря этому, мне не нужно создавать новый вектор и делать много копий вектора
+        // для того, чтобы bfs работал корректно
+        // но, да, мы не можем теперь хранить расстояние в этой ячейке, за полиморфизм нужно платить.
+        // В данном случае это память.
+        graph_[from][to] = to;
+    }
 
-        bool is_splitable = true;
-        for (size_t v = 0; v < amount_vertices_; ++v)
+    const vector<Vertex>& getNeighbors(const Vertex& vertex) const override
+    {
+        return graph_[vertex];
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======GraphAlgorithms=======//
+
+void inputGraphByEdgeList(IGraph& graph)
+{
+    Vertex vertex_a = 0;
+    Vertex vertex_b = 0;
+    for (size_t i = 0; i < graph.getAmountEdges(); ++i)
+    {   
+        cin >> vertex_a >> vertex_b;
+
+        graph.addEdge(vertex_a - 1, vertex_b - 1);
+        graph.addEdge(vertex_b - 1, vertex_a - 1);
+    }
+}
+
+bool isSplitable(Vertex start, const IGraph& graph, vector<Color>& color)
+{
+    size_t is_splitable = true;
+    for (auto& current : graph.getNeighbors(start))
+    {
+        if (color[current] == WHITE)
         {
-            if (color[v] == WHITE) 
-            {
-                color[v] = GRAY;
-                is_splitable = is_splitable && isSplitable(v, color);
-            }
+            color[current] = static_cast<Color>(AMOUNT_COLORS - color[start]);
+            is_splitable = isSplitable(current, graph, color); 
         }
-        if (amount_vertices_ == 1)
+        else if (color[current] == color[start])
         {
             is_splitable = false;
         }
-
-        return is_splitable;
     }
+    return is_splitable;
+}
 
-};
+bool isSplitable(const IGraph& graph)
+{
+    vector<Color> color(graph.getAmountVertices(), WHITE);
 
+    bool is_splitable = true;
+    for (Vertex current = 0; current < graph.getAmountVertices(); ++current)
+    {
+        if (color[current] == WHITE) 
+        {
+            color[current] = GRAY;
+            is_splitable = is_splitable && isSplitable(current, graph, color);
+        }
+    }
+    if (graph.getAmountVertices() == 1)
+    {
+        is_splitable = false;
+    }
+    return is_splitable;
+}
 
 void Processing()
 {
@@ -106,11 +169,11 @@ void Processing()
     size_t amount_edges = 0;
     cin >> amount_vertices >> amount_edges;
 
-    Graph graph(amount_vertices, amount_edges);
+    ListGraph graph(amount_vertices, amount_edges);
 
-    graph.Input();
+    inputGraphByEdgeList(graph);
 
-    cout << (graph.isSplitable() ? "YES" : "NO");
+    cout << (isSplitable(graph) ? "YES" : "NO");
 }
 
 int main()

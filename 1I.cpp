c9baec253f1820473f86e9e1ec9b8ed67a39a933
molidex <kakakakaka2018@mail.ru>
2021-11-22 17:07:51 -0500

    chad I task

--- 1I.cpp
+++ 1I.cpp
@@ -8,55 +8,160 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+typedef int Vertex;
+
 struct Edge
 {   
-    int from;
-    int to;
+    Vertex from;
+    Vertex to;
     int distance;
 };
 
-using Graph = vector<vector<Edge>>;
+const int kInf = (int)1e9;
+const Vertex kStart = -3802;
 
-const int INF = (int)1e9;
+//=======IGraph=======//
 
-void InputGraph(Graph& graph, size_t amount_edges, int& max_distance)
+class IGraph
+{
+public:
+
+    virtual void addEdge(const Vertex& from, const Vertex& to, int distance) = 0;
+    virtual vector<Edge> getNeighbors(const Vertex& vertex) const = 0;
+    virtual size_t getAmountVertices() const = 0;
+    virtual size_t getAmountEdges() const = 0;
+};
+
+//=======ListGraph=======//
+
+class ListGraph : public IGraph
+{
+private:
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+    vector<vector<Edge>> graph_;
+
+public:
+
+    explicit ListGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices_, vector<Edge>())
+        {}
+    
+    void addEdge(const Vertex& from, const Vertex& to, int distance) override
+    {
+        graph_[from].push_back({from, to, distance});
+    }
+
+    vector<Edge> getNeighbors(const Vertex& vertex) const override
+    {
+        return graph_[vertex];
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======MatrixGraph=======//
+
+class MatrixGraph : public IGraph
+{
+private:
+
+    vector<vector<Vertex>> graph_;
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+public:
+
+    explicit MatrixGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices, vector<Vertex>(amount_vertices, kInf))
+        {}
+
+    void addEdge(const Vertex& from, const Vertex& to, int distance) override
+    {   
+        graph_[from][to] = distance;
+    }
+
+    vector<Edge> getNeighbors(const Vertex& vertex) const override
+    {   
+        vector<Edge> neighbors;
+
+        for (Vertex to = 0; to < amount_vertices_; ++to)
+        {   
+            if (graph_[vertex][to] < kInf)
+            {
+                neighbors.push_back({vertex, to, graph_[vertex][to]});
+            }
+        }
+        return neighbors;
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======GraphAlgorithms=======//
+
+void Input(IGraph& graph)
 {
     int distance = 0;
     int vertex_to = 0;
     int vertex_from = 0;
 
-    for (size_t i = 0; i < amount_edges; ++i)
+    for (size_t i = 0; i < graph.getAmountEdges(); ++i)
     {   
         cin >> vertex_from >> vertex_to >> distance;
 
-        if (max_distance < distance) max_distance = distance;
-
-        graph[vertex_from - 1].push_back({vertex_from - 1, vertex_to - 1, distance});
+        graph.addEdge(vertex_from - 1, vertex_to - 1, distance);
     }
 }
 
-int getDistance(Graph& graph, int start, int end, int max_distance)
+int getMaxDistance(IGraph& graph)
+{
+    int max_distance = -1;
+
+    for (Vertex current = 0; current < graph.getAmountVertices(); ++current)
+    {   
+        for (auto& edge : graph.getNeighbors(current))
+        {
+            max_distance = std::max(edge.distance, max_distance);
+        }
+    }
+
+    return max_distance;
+}
+
+int getDistance(IGraph& graph, int start, int end)
 {   
-    vector<int> distances(graph.size(), INF);
+    int max_distance = getMaxDistance(graph);
+
+    vector<int> distances(graph.getAmountVertices(), kInf);
     vector<queue<int>> vector_queue(max_distance + 1);
     
     vector_queue[0].push(start);
     distances[start] = 0;
 
-    int amount_vertices = graph.size();
+    int amount_vertices = graph.getAmountVertices();
     for (int queue_idx = 0; 
          queue_idx < amount_vertices * max_distance + 1; 
          ++queue_idx)
     {   
-        queue<int>& current = vector_queue[queue_idx % (max_distance + 1)];
+        queue<Vertex>& current = vector_queue[queue_idx % (max_distance + 1)];
         while (!current.empty())
         {
-            int front_vertex = current.front();
+            Vertex front_vertex = current.front();
             current.pop();
 
             if (front_vertex == end) return distances[end];
 
-            for (Edge& edge : graph[front_vertex])
+            for (Edge& edge : graph.getNeighbors(front_vertex))
             {
                 if (distances[edge.from] + edge.distance < distances[edge.to])
                 {
@@ -83,10 +188,11 @@ int main()
     cin >> start >> end;
 
     int max_distance = 0;
-    Graph graph(amount_vertices, vector<Edge>());
-    InputGraph(graph, amount_edges, max_distance);
+    ListGraph graph(amount_vertices, amount_edges);
 
-    cout << getDistance(graph, start - 1, end - 1, max_distance);
+    Input(graph);
+
+    cout << getDistance(graph, start - 1, end - 1);
 
     return 0;
 }

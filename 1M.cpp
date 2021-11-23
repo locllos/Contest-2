2cfae75ec13b1824c120d6d46fea41f72dc84e96
molidex <kakakakaka2018@mail.ru>
2021-11-22 20:58:16 -0500

    chad M task

--- 1M.cpp
+++ 1M.cpp
@@ -11,72 +11,153 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-typedef int elem_t;
+using Vertex = int;
 
-const elem_t threshold = -37; 
-
-
-
-void inputGraphByMatrix(size_t amount_edges, 
-                        vector<vector<elem_t>>& graph)
+class IGraph
 {
-    elem_t vertex_a = 0;
-    elem_t vertex_b = 0;
-    for (size_t i = 0; i < amount_edges; ++i)
+public:
+
+    virtual void addEdge(const Vertex& from, const Vertex& to) = 0;
+    virtual vector<Vertex> getNeighbors(const Vertex& vertex) const = 0;
+    virtual size_t getAmountVertices() const = 0;
+    virtual size_t getAmountEdges() const = 0;
+};
+
+//=======ListGraph=======//
+
+class ListGraph : public IGraph
+{
+private:
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+    vector<vector<Vertex>> graph_;
+
+public:
+
+    explicit ListGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices_, vector<Vertex>())
+        {}
+    
+    void addEdge(const Vertex& from, const Vertex& to) override
+    {
+        graph_[from].push_back(to);
+    }
+
+    vector<Vertex> getNeighbors(const Vertex& vertex) const override
+    {
+        return graph_[vertex];
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======MatrixGraph=======//
+
+class MatrixGraph : public IGraph
+{
+private:
+
+    vector<vector<Vertex>> graph_;
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+public:
+
+    explicit MatrixGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices)
+        , amount_edges_(amount_edges)
+        , graph_(amount_vertices, vector<Vertex>(amount_vertices, 0))
+        {}
+
+    void addEdge(const Vertex& from, const Vertex& to) override
     {   
-        cin >> vertex_a >> vertex_b;
-
-        graph[vertex_a - 1][vertex_b - 1] = 1;
+        graph_[from][to] = 1;
     }
-}
 
-void inputGraphByList(size_t amount_edges, 
-                      vector<vector<elem_t>>& graph)
+    vector<Vertex> getNeighbors(const Vertex& vertex) const override
+    {
+        vector<Vertex> neighbors;
+
+        for (Vertex to = 0; to < amount_vertices_; ++to)
+        {   
+            if (graph_[vertex][to] != 0)
+            {
+                neighbors.push_back(to);
+            }
+        }
+        return neighbors;
+    }
+
+    size_t getAmountVertices() const {return amount_vertices_;};
+    size_t getAmountEdges() const {return amount_edges_;};
+};
+
+//=======GraphAlgorithms=======//
+
+
+void inputGraph(IGraph& graph)
 {
-    elem_t vertex_a = 0;
-    elem_t vertex_b = 0;
-    for (size_t i = 0; i < amount_edges; ++i)
+    Vertex vertex_a = 0;
+    Vertex vertex_b = 0;
+    for (size_t i = 0; i < graph.getAmountEdges(); ++i)
     {
         cin >> vertex_a >> vertex_b;
 
-        graph[vertex_a - 1].push_back(vertex_b - 1);
-        graph[vertex_b - 1].push_back(vertex_a - 1);
+        graph.addEdge(vertex_a - 1, vertex_b - 1);
+        graph.addEdge(vertex_b - 1, vertex_a - 1);
     }
 }
 
-void DFS(elem_t start_vertex, 
-         vector<bool>& visited, vector<vector<elem_t>>& graph, stack<elem_t>& connected_vertices)
+void DFS(const IGraph& graph, vector<bool>& visited, vector<Vertex>& connected_vertices, const Vertex& start)
 {
-    visited[start_vertex] = true;
-    for (elem_t current_vertex : graph[start_vertex])
+    visited[start] = true;
+    for (Vertex& current : graph.getNeighbors(start))
     {
-        if (!visited[current_vertex])
-        {
-            DFS(current_vertex, visited, graph, connected_vertices);
+        if (!visited[current])
+        {   
+            DFS(graph, visited, connected_vertices, current);
         }
     }
-    connected_vertices.push(start_vertex);
+    connected_vertices.push_back(start);
 }
 
-
-void printAnswer(queue<stack<elem_t>>& components) 
+void printAnswer(const vector<vector<Vertex>>& components) 
 {
     cout << components.size() << endl;
 
-    while (!components.empty()) 
+    for (auto& component : components)
     {
-        stack<elem_t>& connected_vertices = components.front();
-
-        cout << connected_vertices.size() << endl;
-        while (!connected_vertices.empty()) 
+        cout << component.size() << endl;
+        
+        for (int i = component.size() - 1; i > -1; --i)
         {
-            cout << connected_vertices.top() + 1 << ' ';
-
-            connected_vertices.pop();
+            cout << component[i] + 1 << ' ';
         }
         cout << endl;
-        components.pop();
     }
+}
+
+vector<vector<Vertex>> getComponents(const IGraph& graph)
+{
+    vector<bool> visited(graph.getAmountVertices(), false);
+    vector<vector<Vertex>> components;
+    for (Vertex current = 0; current < graph.getAmountVertices(); ++current)
+    {
+        if (!visited[current])
+        {
+            components.push_back(vector<Vertex>());
+
+            DFS( graph, visited, components.back(), current);
+        }
+    }
+
+    return components;
 }
 
 void Processing()
@@ -85,21 +166,11 @@ void Processing()
     size_t amount_edges = 0;
     cin >> amount_vertices >> amount_edges;
 
-    vector<vector<elem_t>> graph(amount_vertices);
+    ListGraph graph(amount_vertices, amount_edges);
+    inputGraph(graph);
 
-    inputGraphByList(amount_edges, graph);
+    vector<vector<Vertex>> components = getComponents(graph);
 
-    vector<bool> visited(amount_vertices, false);
-    queue<stack<elem_t>> components;
-    for (size_t i = 0; i < amount_vertices; ++i)
-    {
-        if (!visited[i])
-        {
-            components.push(stack<elem_t>());
-
-            DFS(i, visited, graph, components.back());
-        }
-    }
     printAnswer(components);
 
 }

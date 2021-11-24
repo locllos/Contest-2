6b0191d17bc0828a2d1f8d687dfb60a027b97399
molidex <kakakakaka2018@mail.ru>
2021-11-23 22:11:03 -0500

    super chad C task

--- 1C.cpp
+++ 1C.cpp
@@ -1,160 +1,246 @@
 #include <iostream>
 #include <queue>
 #include <vector>
+#include <algorithm>
 
-using std::vector;
-using std::queue;
 using std::cin;
 using std::cout;
+using std::queue;
+using std::vector;
 
-const size_t INF = -1;
+using Vertex = int;
+
+const size_t kInf = -1;
+const Vertex kStart = -3802;
 
 struct ChessPosition
 {
-    size_t x;
-    size_t y;
+    int x;
+    int y;
 
-    void setChessPathLength(vector<vector<size_t>>& data, size_t length)
-    {
-        data[x][y] = length;
-    }
-
-    size_t getChessPathLength(const vector<vector<size_t>>& data)
-    {
-        return data[x][y];
-    }
-
-    bool operator==(const ChessPosition& other)
+    bool operator==(const ChessPosition &other)
     {
         return (other.x == x && other.y == y);
     }
 
-    const ChessPosition operator+(const ChessPosition& other)
+    ChessPosition operator+(const ChessPosition &other)
     {
         return {x + other.x, y + other.y};
     }
 
-    const ChessPosition operator-(const ChessPosition& other)
+    ChessPosition operator-(const ChessPosition &other)
     {
         return {x - other.x, y - other.y};
     }
+
+    int getUniqueNumber(int row_length)
+    {
+        return row_length * y + x;
+    }
 };
 
-using chess_queue = queue<ChessPosition>;
-
-void Processing(vector<vector<chess_queue>>& paths,
-                const ChessPosition& from, const ChessPosition& to)
+ChessPosition extractChessPosition(int unique_number, int row_length)
 {
-    chess_queue queue;
-    size_t chess_size = paths.size();
+    return {unique_number % row_length, unique_number / row_length};
+}
 
-    queue.push(from);
 
+const ChessPosition kHorseMoves[] = {{2, 1}, {1, 2}, {2, -1}, {1, -2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}};
+
+//=======IGraph=======//
+
+enum Color
+{
+    WHITE,
+    GRAY,
+    BLACK,
+};
+
+class IGraph
+{
+public:
+    virtual void addEdge(const Vertex &from, const Vertex &to) = 0;
+    virtual vector<Vertex> getNeighbors(const Vertex &vertex) const = 0;
+    virtual size_t getAmountVertices() const = 0;
+    virtual size_t getAmountEdges() const = 0;
+};
+
+//=======ListGraph=======//
+
+class ListGraph : public IGraph
+{
+private:
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+    vector<vector<Vertex>> graph_;
+
+public:
+    explicit ListGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices), amount_edges_(amount_edges), graph_(amount_vertices_, vector<Vertex>())
+    {
+    }
+
+    void addEdge(const Vertex &from, const Vertex &to) override
+    {
+        graph_[from].push_back(to);
+        ++amount_edges_;
+    }
+
+    vector<Vertex> getNeighbors(const Vertex &vertex) const override
+    {
+        return graph_[vertex];
+    }
+
+    size_t getAmountVertices() const { return amount_vertices_; };
+    size_t getAmountEdges() const { return amount_edges_; };
+};
+
+//=======MatrixGraph=======//
+
+class MatrixGraph : public IGraph
+{
+private:
+    vector<vector<Vertex>> graph_;
+
+    size_t amount_vertices_;
+    size_t amount_edges_;
+
+public:
+    explicit MatrixGraph(size_t amount_vertices, size_t amount_edges)
+        : amount_vertices_(amount_vertices), amount_edges_(amount_edges), graph_(amount_vertices, vector<Vertex>(amount_vertices, 0))
+    {}
+
+    void addEdge(const Vertex &from, const Vertex &to) override
+    {
+        graph_[from][to] = 1;
+        ++amount_edges_;
+    }
+
+    vector<Vertex> getNeighbors(const Vertex &vertex) const override
+    {
+        vector<Vertex> neighbors;
+
+        for (Vertex to = 0; to < amount_vertices_; ++to)
+        {
+            if (graph_[vertex][to] != 0)
+            {
+                neighbors.push_back(to);
+            }
+        }
+        return neighbors;
+    }
+
+    size_t getAmountVertices() const { return amount_vertices_; };
+    size_t getAmountEdges() const { return amount_edges_; };
+};
+
+void inputGraph(IGraph &graph, int chess_size)
+{   
+    ChessPosition pos = {0, 0};
+    for (; pos.x < chess_size; ++pos.x)
+    {
+        for (; pos.y < chess_size; ++pos.y)
+        {
+            for (auto &move : kHorseMoves)
+            {
+                if (pos.x + move.x < chess_size && pos.x + move.x > -1 &&
+                    pos.y + move.y < chess_size && pos.y + move.y > -1)
+                {
+                    ChessPosition result = pos + move;
+                    graph.addEdge(pos.getUniqueNumber(chess_size),
+                                  result.getUniqueNumber(chess_size));
+                }
+            }
+        }
+    }
+}
+
+void BFS(const IGraph& graph, Vertex start, vector<bool>& visited, vector<Vertex>& parents)
+{
+    queue<Vertex> queue;
+
+    visited[start] = true;
+    parents[start] = kStart;
+    
+    queue.push(start);
     while (!queue.empty())
     {
-        ChessPosition current = queue.front();
-
+        Vertex current = queue.front();
         queue.pop();
 
-        if (current == to) break;
+        for (Vertex to : graph.getNeighbors(current))
+        {
+            if (!visited[to])
+            {
+                visited[to] = true;
 
-        if (current.x < chess_size - 2 && 
-            current.y < chess_size - 1 &&
-            paths[current.x + 2][current.y + 1].size() == 0) 
-        {
-            paths[current.x + 2][current.y + 1] = paths[current.x][current.y];
-            paths[current.x + 2][current.y + 1].push(current);
-            queue.push(ChessPosition({current.x + 2, current.y + 1}));
-        }
-        if (current.x < chess_size - 1 && 
-            current.y < chess_size - 2 &&
-            paths[current.x + 1][current.y + 2].size() == 0) 
-        {
-            paths[current.x + 1][current.y + 2] = paths[current.x][current.y];
-            paths[current.x + 1][current.y + 2].push(current);
-            queue.push(ChessPosition({current.x + 1, current.y + 2}));
-        }
-        if (current.x < chess_size - 2 && 
-            current.y > 0 &&
-            paths[current.x + 2][current.y - 1].size() == 0) 
-        {
-            paths[current.x + 2][current.y - 1] = paths[current.x][current.y];
-            paths[current.x + 2][current.y - 1].push(current);
-            queue.push(ChessPosition({current.x + 2, current.y - 1}));
-        }
-        if (current.x > 0 && 
-            current.y < chess_size - 2 &&
-            paths[current.x - 1][current.y + 2].size() == 0) 
-        {
-            paths[current.x - 1][current.y + 2] = paths[current.x][current.y];
-            paths[current.x - 1][current.y + 2].push(current);
-            queue.push(ChessPosition({current.x - 1, current.y + 2}));
-        }
-        if (current.x > 1 && 
-            current.y < chess_size - 1 &&
-            paths[current.x - 2][current.y + 1].size() == 0) 
-        {
-            paths[current.x - 2][current.y + 1] = paths[current.x][current.y];
-            paths[current.x - 2][current.y + 1].push(current);
-            queue.push(ChessPosition({current.x - 2, current.y + 1}));
-        }
-        if (current.x < chess_size - 1 && 
-            current.y > 1 &&
-            paths[current.x + 1][current.y - 2].size() == 0) 
-        {
-            paths[current.x + 1][current.y - 2] = paths[current.x][current.y];
-            paths[current.x + 1][current.y - 2].push(current);
-            queue.push(ChessPosition({current.x + 1, current.y - 2}));
-        }
-        if (current.x > 0 && 
-            current.y > 2 &&
-            paths[current.x - 1][current.y - 2].size() == 0) 
-        {
-            paths[current.x - 1][current.y - 2] = paths[current.x][current.y];
-            paths[current.x - 1][current.y - 2].push(current);
-            queue.push(ChessPosition({current.x - 1, current.y - 2}));
-        }
-        if (current.x > 2 && 
-            current.y > 1 &&
-            paths[current.x - 2][current.y - 1].size() == 0) 
-        {
-            paths[current.x - 2][current.y - 1] = paths[current.x][current.y];
-            paths[current.x - 2][current.y - 1].push(current);
-            queue.push(ChessPosition({current.x - 2, current.y - 1}));
+                queue.push(to);
+                
+                parents[to] = current;
+            }
         }
     }
-}   
+}
+
+void initOptimalPath(Vertex target,
+                     vector<Vertex>& path,
+                     const vector<Vertex>& parents)
+{   
+    Vertex current = target;
+    while (current != kStart)
+    {
+        path.push_back(current);
+        current = parents[current];
+    }
+
+    std::reverse(path.begin(), path.end());
+}
+
+vector<Vertex> getOptimalPath(const IGraph& graph, Vertex start, Vertex end)
+{
+    vector<bool> visited(graph.getAmountVertices());
+    vector<Vertex> parents(graph.getAmountVertices());
+    vector<Vertex> path;
+
+    BFS(graph, start, visited, parents);
+
+    if (visited[end])
+    {
+        initOptimalPath(end, path, parents);
+    }
+    return path;
+}
 
 int main()
-{   
+{
     size_t chess_size;
 
-    ChessPosition pos_from;
-    ChessPosition pos_to;
+    ChessPosition start;
+    ChessPosition end;
 
     cin >> chess_size;
-    cin >> pos_from.x >> pos_from.y;
-    cin >> pos_to.x >> pos_to.y;
+    cin >> start.x >> start.y;
+    cin >> end.x >> end.y;
 
-    pos_from.x -= 1;
-    pos_from.y -= 1;
+    start.x -= 1;
+    start.y -= 1;
 
-    pos_to.x -= 1;
-    pos_to.y -= 1;
+    end.x -= 1;
+    end.y -= 1;
 
-    vector<vector<chess_queue>> paths(chess_size, vector<chess_queue>(chess_size));
-    Processing(paths, pos_from, pos_to);
+    ListGraph graph(chess_size * chess_size, 0);
+    inputGraph(graph, chess_size);
 
-    cout << paths[pos_to.x][pos_to.y].size() << std::endl;
-    while (!paths[pos_to.x][pos_to.y].empty())
-    {   
-        ChessPosition current = paths[pos_to.x][pos_to.y].front();
-        
-        cout << current.x + 1 << " " << current.y + 1 << std::endl;
+    vector<Vertex> path = getOptimalPath(graph, start.getUniqueNumber(chess_size),
+                                                end.getUniqueNumber(chess_size));
+    cout << path.size() << '\n';
+    for (auto& number : path)
+    {
+        ChessPosition pos = extractChessPosition(number, chess_size);
 
-        paths[pos_to.x][pos_to.y].pop();
+        cout << pos.x + 1 << ' ' << pos.y + 1 << '\n';
     }
-    cout << pos_to.x + 1 << " " << pos_to.y + 1 << std::endl;
 
     return 0;
 }
